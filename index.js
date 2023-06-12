8a38d299b0e90f4fd38255964d391fc24bcced8c
barakatimothy <timothybaraka39@gmail.com>
2023-06-12 11:59:31 -0400

    complete!

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+ 
+  if (someValue <= 400){
+    return "This one is on me!";
+  }
+  else if (someValue >2000 && someValue < 2500){
+    return 'I will gladly take your thirty bucks.';
+  }
+  else 
+     return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   if (city ==="NYC"){
+    return 'Ok, sounds good.';
+   }
+   else {
+     return "No go."
+   }
+}
+function switchOnCharmFromTip(tip){
+  if (tip == "generous"){
+    return "Thank you so much.";
+  }
+  else if (tip  == "not as generous"){
+    return "Thank you.";
+  }
+  else 
+    return "Bye.";   
 }
